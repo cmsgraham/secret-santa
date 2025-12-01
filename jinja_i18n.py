10b67676510b14bd2f6a3a262778f70c60f93e5f
Cristian Madrigal <cristian.madrigal@gmail.com>
2025-12-01 17:47:19 -0600

    Make translation functions request-aware to use current locale from g.locale instead of static startup locale

--- jinja_i18n.py
+++ jinja_i18n.py
@@ -3,7 +3,8 @@ Jinja2 filters and globals for i18n integration
 """
 
 from jinja2 import Environment
-from i18n import get_translation, I18nContext
+from flask import g
+from i18n import get_translation, I18nContext, DEFAULT_LOCALE
 
 
 def add_i18n_to_jinja(app, locale='en'):
@@ -11,18 +12,33 @@ def add_i18n_to_jinja(app, locale='en'):
     Add i18n functions and filters to Jinja2 environment
     """
 
-    # Create translation context
-    i18n_ctx = I18nContext(locale)
+    # Create default translation context (for fallback only)
+    default_i18n_ctx = I18nContext(locale)
 
-    # Register as global function
-    app.jinja_env.globals['t'] = i18n_ctx.get
-    app.jinja_env.globals['_'] = i18n_ctx.get  # Shorthand
+    # Create request-aware translation functions
+    def get_current_locale():
+        """Get the current locale from Flask's request context"""
+        return g.get('locale', DEFAULT_LOCALE)
+
+    def t_func(key, **kwargs):
+        """Translate using current request's locale"""
+        current_locale = get_current_locale()
+        return get_translation(key, current_locale, **kwargs)
+
+    def _func(key, **kwargs):
+        """Shorthand for t_func"""
+        return t_func(key, **kwargs)
+
+    # Register as global functions (these will be called at render time with current locale)
+    app.jinja_env.globals['t'] = t_func
+    app.jinja_env.globals['_'] = _func
     # NOTE: 'locale' and 'SUPPORTED_LOCALES' are now injected per-request via context_processor
     # Do NOT set them as static globals here, as they would override context_processor values
 
     # Register filters
     def translate(key, **kwargs):
-        return i18n_ctx.get(key, **kwargs)
+        """Filter for translations - uses current request's locale"""
+        return t_func(key, **kwargs)
 
     def language_name(locale_code):
         """Get native name of language"""
@@ -40,7 +56,7 @@ def add_i18n_to_jinja(app, locale='en'):
     app.jinja_env.filters['language_name'] = language_name
     app.jinja_env.filters['language_flag'] = language_flag
 
-    return i18n_ctx
+    return default_i18n_ctx
 
 
 def create_language_selector_macro():
